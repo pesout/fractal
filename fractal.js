30d12955e8515727c7cdb4f5d787bc9da4935558
Štěpán Pešout <stepan@pesout.eu>
2018-11-19 16:59:08 +0100

    Update fractal.js

--- fractal.js
+++ fractal.js
@@ -1,106 +1,68 @@
-/*
-************************
-Created by Stepan Pesout
-*****www.pesout.eu******
-*****@stepanpesout******
-************************
-*/
+var a, b, canvas, draw, rep, sx, sy, x, y;
 
-var canvas = document.getElementById("myCanvas");
-canvas.width = window.innerWidth; //The canvas uses the whole browser window
+canvas = document.getElementById('myCanvas');
+
+draw = function() {
+  var a, anahoda, b, bnahoda, context, elem, x, xnahoda, y, ynahoda;
+  xnahoda = Math.round(Math.random());
+  ynahoda = Math.round(Math.random());
+  anahoda = Math.round(Math.random());
+  bnahoda = Math.round(Math.random());
+  if (xnahoda === 1 && x > 0) {
+    x = x - 1;
+  } else {
+    if (x < sx) {
+      x = x + 1;
+    }
+  }
+  if (ynahoda === 1 && y > 0) {
+    y = y - 1;
+  } else {
+    if (y < sy) {
+      y = y + 1;
+    }
+  }
+  if (anahoda === 1) {
+    a = x + 1;
+  } else {
+    a = x - 1;
+  }
+  if (bnahoda === 1) {
+    b = y + 1;
+  } else {
+    b = y - 1;
+  }
+  elem = document.getElementById('myCanvas');
+  context = elem.getContext('2d');
+  context.strokeStyle = '#00f';
+  if (ynahoda === 1 || xnahoda === 1) {
+    context.strokeStyle = '#0f0';
+  }
+  context.lineWidth = 5;
+  context.beginPath();
+  context.moveTo(x, y);
+  context.lineTo(a, b);
+  context.stroke();
+};
+
+rep = function() {
+  window.setInterval('draw()', 1);
+};
+
+canvas.width = window.innerWidth;
+
 canvas.height = window.innerHeight;
 
+x = Math.round(window.innerWidth / 2);
 
-//Center of the page
-var x = Math.round(window.innerWidth/2); 
-var y = Math.round(window.innerHeight/2);
+y = Math.round(window.innerHeight / 2);
 
+a = x;
 
-var a = x;
-var b = y;
+b = y;
 
+sx = x * 2;
 
-var sx = x*2;
-var sy = y*2;
+sy = y * 2;
 
-
-function draw() {
-	
-	//Variables with random values declatarion
-	xnahoda = Math.round(Math.random());
-	ynahoda = Math.round(Math.random());
-	anahoda = Math.round(Math.random());
-	bnahoda = Math.round(Math.random());
-
-	//Drawing, which depends on previous code
-	if (xnahoda == 1 && x > 0)
-	{
-		x = x - 1;
-	}
-	
-	else
-	{
-		if (x < sx) 
-		{
-			x = x + 1;
-		}
-	}
-	
-	if (ynahoda == 1 && y > 0)
-	{
-		y = y - 1;
-	} 
-	else 
-	{
-		if (y < sy) 
-		{
-			y = y + 1;
-		}
-	}
-	
-	if (anahoda == 1) 
-	{
-		a = x + 1;
-	} 
-	else 
-	{
-		a = x - 1;
-	}
-	
-	if (bnahoda == 1) 
-	{
-		b = y + 1;
-	} 
-	else 
-	{
-		b = y - 1;
-	}
-
-	var elem = document.getElementById('myCanvas');
-	var context = elem.getContext('2d');
-	
-	//Color changing for fun
-	context.strokeStyle = '#00f'
-	if (ynahoda == 1 || xnahoda == 1) 
-	{
-		context.strokeStyle = '#0f0';
-	}
-	
-	//Line render
-	context.lineWidth = 5;
-	context.beginPath();
-	context.moveTo(x, y);
-	context.lineTo(a, b);
-	context.stroke();
-
-}
-
-
-//Function of repeating every millisecond
-function rep() 
-{
-	window.setInterval("draw()", 1);
-}
-
-//Repeating of repeating :-)
-window.setInterval("rep()", 1);
+window.setInterval('rep()', 1);
